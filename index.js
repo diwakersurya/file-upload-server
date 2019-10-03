47b0cdaa6159e06d1ae1324385c39f8047c7a1d7
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-03 15:22:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+// default options
+app.use(fileUpload());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/', function (req, res) {
+    res.json({ success: true })
+})
+app.post('/upload', function (req, res) {
+    console.log(">>>>>>>", req.files);
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    // Use the mv() method to place the file somewhere on your server
+    req.files.fileData.mv('files/' + req.files.fileData.name, function (err) {
+        if (err) {
+            console.error(err)
+            return res.status(500).send(err);
+        }
+        res.json({ success: true });
+    });
+});
+const port = 5000
+app.listen(port, () => console.log(`file server listening on port ${port}!`))
