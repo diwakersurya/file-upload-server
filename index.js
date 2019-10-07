fedf059898aa335ec83569829e925cc1eabfcc0e
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-07 14:29:13 +0530

    wip

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const path=require('path');
 const app = express();
+const publicDir=path.resolve(__dirname +'/public/');
 // default options
+app.use(express.static(publicDir));
 app.use(fileUpload());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,13 +17,16 @@ app.post('/upload', function (req, res) {
         return res.status(400).send('No files were uploaded.');
     }
     // Use the mv() method to place the file somewhere on your server
-    req.files.fileData.mv('public/' + req.files.fileData.name, function (err) {
+    req.files.data.mv(`${publicDir}/${req.files.data.name}`, function (err) {
         if (err) {
             console.error(err)
             return res.status(500).send(err);
         }
         res.json({ success: true });
     });
+});
+app.get('/', function (req, res) {
+     res.sendfile(__dirname + '/index.html');
 });
 const port = 5000
 app.listen(port, () => console.log(`file server listening on port ${port}!`))
